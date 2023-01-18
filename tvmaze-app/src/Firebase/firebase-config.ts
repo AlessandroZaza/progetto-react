69b95388d39b6ce5ea252375870bf565ba57fe69
AlessandroZaza <alessandro.zaza@edu.itspiemonte.it>
2023-01-18 15:38:22 +0100

    installato firebase

---
+++ tvmaze-app/src/Firebase/firebase-config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD7NCUKPHWSJDswzlp2N9A9YuH1rJi-WMA",
+  authDomain: "tvmaze-app-9b776.firebaseapp.com",
+  projectId: "tvmaze-app-9b776",
+  storageBucket: "tvmaze-app-9b776.appspot.com",
+  messagingSenderId: "679366112719",
+  appId: "1:679366112719:web:9ac6d383d23aa076470eba"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
