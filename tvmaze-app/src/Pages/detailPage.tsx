d5ce82e729248dd8012e304995d90b5a482d716e
alessandrozaza <alessandro.zaza@edu.itspiemonte.com>
2023-01-24 10:37:41 +0100

    bug fixed

--- tvmaze-app/src/Pages/detailPage.tsx
+++ tvmaze-app/src/Pages/detailPage.tsx
@@ -3,7 +3,6 @@ import Footer from '../Components/footer'
 import MovieCard from '../Components/movieCard'
 import CardList from '../Components/cardList';
 import React from 'react';
-import DetailPage from '../Components/detailPage';
 
 const DetailPages = () => {
 
@@ -13,11 +12,6 @@ const DetailPages = () => {
 
 <div style={{backgroundColor: 'white'}}>
 
-        <DetailPage match={{
-                params: {
-                    id: ''
-                }
-            }} />
             
 </div>
         <Footer></Footer>
